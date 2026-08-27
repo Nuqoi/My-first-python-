9e68ab2dcc77335e77b79f7f0eb38a9ae74ebf5d
Nuqoi <romaissali2011@gmail.com>
2026-08-27 22:36:37 +0100

    Add files via upload

---
+++ chrono project .py
@@ -0,0 +1,17 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> number = int(input("Enter a number: "))
+Enter a number: 5
+>>> for i in range(5, -1, -1):
+...     print(i)
+... else:
+...     print("Time has done")
+... 
+...     
+5
+4
+3
+2
+1
+0
+Time has done
