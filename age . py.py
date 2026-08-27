8db235267f16032d491ef381b8f969255533a1b0
Nuqoi <romaissali2011@gmail.com>
2026-08-27 15:54:02 +0100

    Add files via upload

---
+++ age . py.py
@@ -0,0 +1,15 @@
+>>> age =int(input("Enter your age: "))
+Enter your age: 15
+>>> if age <=13:
+...     print("Child")
+... elif age <=19:
+...     print("Teenager")
+... elif age <=59:
+...     print("Adult")
+... else:
+...     print("Senior")
+... print("Enjoy your life")
+...
+Teenager
+Enjoy your life
+>>>
