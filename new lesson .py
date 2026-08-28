dac5d5bd72c69699d3a5d181597f1e4047978601
Nuqoi <romaissali2011@gmail.com>
2026-08-28 22:16:59 +0100

    Add files via upload

---
+++ new lesson .py
@@ -0,0 +1,13 @@
+ligne = int(input("Enter your ligne's number:"))
+Enter your ligne's number:3
+colonne = int(input("Enter your colonne's number:"))
+Enter your colonne's number:5
+for i in range(ligne):
+    for j in range(colonne):
+        print("*", end=" ")
+    print()
+
+    
+* * * * * 
+* * * * * 
+* * * * * 
