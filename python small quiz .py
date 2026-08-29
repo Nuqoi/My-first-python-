860fb298278048ac58abd54ecc68c279587ac18b
Nuqoi <romaissali2011@gmail.com>
2026-08-29 16:07:44 +0100

    Add files via upload

---
+++ python small quiz .py
@@ -0,0 +1,42 @@
+x = 7
+y = 3
+print(x + y) ; print(x * y)
+10
+21
+name = "Roumaissae"
+print("Hello" , name) ; print(name * 3)
+Hello Roumaissae
+RoumaissaeRoumaissaeRoumaissae
+x = 8
+if x < 5:
+     print("Small")
+else:
+     print("Big")
+
+     
+Big
+print(x)
+8
+age = 14
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+    
+Minor
+print(age + 1)
+15
+x = 5
+if x > 3:
+    print("A")
+else:
+     print("B")
+
+ A
+     x = 4
+if x == 4:
+    print("Yes")
+
+    
+Yes
