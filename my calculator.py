00b0770e657089a523454a4278a34b6eeca86507
Nuqoi <romaissali2011@gmail.com>
2026-09-05 15:41:49 +0100

    Add files via upload

---
+++ my calculator.py
@@ -0,0 +1,21 @@
+print("=== Calculator ===")
+=== Calculator ===
+num1 = float(input("Enter the first number: "))
+Enter the first number: 10
+num2 = float(input("Enter the second number: "))
+Enter the second number: 5
+operation = input("Choose (+, -, *, /): ")
+Choose (+, -, *, /): +
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif opertaion == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operation")
+
+    
+15.0
